3edfb9b5b1c65550949cfd2733129a225c6cad7e
yoheng2016 <529858440@qq.com>
2021-01-09 22:47:03 +0800

    Update zframework.js

--- zframework.js
+++ zframework.js
@@ -1,7 +1,7 @@
-﻿(function (w) {
+(function (w) {
     //基础开发库
-    //每修改一次,版本加1
-    //版本：8 
+    //每修改一次,版本加0.1
+    //版本：0.2
 
     //已有类型：object相关、array相关
 
@@ -13,10 +13,9 @@
     function Support() {
 
     }
-    //------------------1.object相关---开始----------------------
-    Support.prototype.obj = function () {
 
-    }
+    //------------------1.object相关---开始----------------------
+    Support.prototype.obj = {}
     //1-1.对象是否为空-赵恒
     Support.prototype.obj.isNull = function (obj) {
         return obj == null;
@@ -41,9 +40,7 @@
     //------------------object相关---结束----------------------
 
     //------------------2.array相关---开始----------------------
-    Support.prototype.array = function () {
-
-    }
+    Support.prototype.array = {}
     //2-1.数组对象是否为空或为长度为0--赵恒
     Support.prototype.array.isNullOrZero = function (arr) {
         var flag = false;
@@ -66,7 +63,7 @@
         if (!this.isArray(arr)) {
             throw new Error("该入参不是数组类型！");
         }
-        ary.splice(0, ary.length);
+        arr.splice(0, ary.length);
     }
     //2-4.获取最大值--赵恒
     Support.prototype.array.max = function (arr) {
@@ -79,7 +76,7 @@
         }
         for (var i = 0; i < arr.length; i++) {
             var item = arr[i];
-            if (!isNaN(item)) {
+            if (isNaN(item)) {
                 throw new Error("数组内存在非数字的值！");
             }
             if ((val == null) || (item > val)) {
@@ -89,7 +86,7 @@
         return val;
     }
     //2-5.获取最小值--赵恒
-    Support.prototype.array.max = function (arr) {
+    Support.prototype.array.min = function (arr) {
         if (!this.isArray(arr)) {
             throw new Error("该入参不是数组类型！");
         }
@@ -99,7 +96,7 @@
         }
         for (var i = 0; i < arr.length; i++) {
             var item = arr[i];
-            if (!isNaN(item)) {
+            if (isNaN(item)) {
                 throw new Error("数组内存在非数字的值！");
             }
             if ((val == null) || (item < val)) {
@@ -172,7 +169,7 @@
             if (item == null) {
                 continue;
             }
-            if (!isNaN(item)) {
+            if (isNaN(item)) {
                 throw new Error("数组内元素存在非数字的值！");
             }
             sum += item;
@@ -191,7 +188,7 @@
             if (val == null) {
                 continue;
             }
-            if (!isNaN(val)) {
+            if (isNaN(val)) {
                 throw new Error("数组聚合字段存在非数字的值！");
             }
             sum += val;
@@ -268,8 +265,176 @@
         var val = sum / count;
         return val;
     }
-
+    //2-16.根据索引获取元素--赵恒 
+    Support.prototype.array.get = function (arr, num) {
+        if (!this.isArray(arr)) {
+            throw new Error("该入参不是数组类型！");
+        }
+        var count = arr.length;
+        if (num > count) {
+            return null;
+        }
+        var obj = arr[num];
+        return obj;
+    }
+    //2-17.根据指定字段获取最大值元素--赵恒
+    Support.prototype.array.maxBy = function (arr, callBack) {
+        if (!this.isArray(arr)) {
+            throw new Error("该入参不是数组类型！");
+        }
+        var val;
+        if (this.isNullOrZero(arr)) {
+            return val;
+        }
+        for (var i = 0; i < arr.length; i++) {
+            var item = arr[i];
+           var num = callBack(item);
+           if (isNaN(num)) {
+                throw new Error("指定字段非数字类型！");
+            }
+           if ((val == null) || (num > val)) {
+               val = num;
+            }
+        }
+        return val;
+    }
+    //2-18.根据指定字段获取最小值元素--赵恒
+    Support.prototype.array.minBy = function (arr, callBack) {
+        if (!this.isArray(arr)) {
+            throw new Error("该入参不是数组类型！");
+        }
+        var val;
+        if (this.isNullOrZero(arr)) {
+            return val;
+        }
+        for (var i = 0; i < arr.length; i++) {
+            var item = arr[i];
+            var num = callBack(item);
+            if (isNaN(num)) {
+                throw new Error("数组内存在非数字的值！");
+            }
+            if ((val == null) || (num < val)) {
+                val = num;
+            }
+        }
+        return val;
+    }
     //------------------array相关---结束----------------------
 
-    w.$z = w.$framework = factory();
+    //------------------3.date相关---开始----------------------
+    Support.prototype.date = {};
+    //3.1将日期时间转换为指定格式，如：YYYY-mm-dd HH:MM表示2019-06-06 19:45
+    Support.prototype.date.dateFormat = function (fmt, date) {
+        let ret;
+        const opt = {
+            "Y+": date.getFullYear().toString(),        // 年
+            "m+": (date.getMonth() + 1).toString(),     // 月
+            "d+": date.getDate().toString(),            // 日
+            "H+": date.getHours().toString(),           // 时
+            "M+": date.getMinutes().toString(),         // 分
+            "S+": date.getSeconds().toString()          // 秒
+            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+        };
+        for (let k in opt) {
+            ret = new RegExp("(" + k + ")").exec(fmt);
+            if (ret) {
+                fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+            };
+        };
+        return fmt;
+    }
+    //3.2获取当前短日期YYYY-mm-dd
+    Support.prototype.date.getDate = function () {
+        var date = new Date()
+        var fmtDate = this.dateFormat("YYYY-mm-dd", date);
+        return fmtDate;
+    }
+    //3.3获取当前长日期YYYY-mm-dd HH:MM:SS
+    Support.prototype.date.getLongTime = function () {
+        var date = new Date()
+        var fmtDate = this.dateFormat("YYYY-mm-dd HH:MM:SS", date);
+        return fmtDate;
+    }
+    //------------------date相关---结束----------------------
+    //------------------4.函数相关---开始----------------------
+    //4.1对象或数组循环
+    Support.prototype.each = function (obj, callBack) {
+        var isArray = Array.isArray(obj);
+        var arr = obj;
+        if (isArray) {
+            for (var i = 0; i < arr.length; i++) {
+                var item = arr[i];
+                callBack(item, i);
+            }
+        }
+        if (typeof obj == "object") {
+            for (let item in obj) {
+                callBack(item, i);
+            }
+        }
+    }
+    //4.2迭代器
+    Support.prototype.iterator = function (arr) {
+        return new Iterator(arr);
+    }
+    function Iterator(arr) {
+        this.arr = arr;
+        this.index = 0;
+        var indexLength = this.arr.length;
+        this.hasNext = function () {
+            return this.index < indexLength;
+        }
+        this.next = function () {
+            var obj = this.arr[this.index];
+            this.index++;
+            return obj;
+        }
+        this.getIndex = function () {
+            return this.index;
+        }
+    }
+    //4.3传输对象管理器
+    Support.prototype.transfer = function () {
+        return new Transfer();
+    }
+    function Transfer() {
+        this.arr = [];
+        this.insert = function (element) {
+            this.arr.push(element);
+        }
+        this.remove = function (element) {
+            for (var i = 0; i < this.arr.length; i++) {
+                var item = this.arr[i];
+                if (element === item) {
+                    this.arr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        this.modify = function (element, obj) {
+            for (var i = 0; i < this.arr.length; i++) {
+                var item = this.arr[i];
+                if (element === item) {
+                    this.arr[i] = obj;
+                    break;
+                }
+            }
+        }
+        this.execute = function (callBack) {
+            for (var i = 0; i < this.arr.length; i++) {
+                var item = this.arr[i];
+                callBack(item);
+            }
+        }
+        this.getArray = function () {
+            return this.arr;
+        }
+        this.clear = function () {
+            this.arr.splice(0, this.arr.length);
+        }
+
+    }
+    //------------------函数相关---结束----------------------
+
+    w.$z = w.$zframework = factory();
 })(window);
